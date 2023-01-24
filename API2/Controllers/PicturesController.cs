5e3f01a1638f0d72abe81bb61544490b418d84e2
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-01-24 18:13:59 +0300

    API 2 için ilk izinli örnek uygulandı.

---
+++ API2/Controllers/PicturesController.cs
@@ -0,0 +1,24 @@
+﻿using API2.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        [Authorize]
+        public IActionResult GetPictures()
+        {
+            var pictures = new List<Picture>()
+            {
+                new Picture{Id=1,Name="Doğa Resmi", Url="Dogaresmi.jpg"},
+                new Picture{Id=1,Name="Uzay Resmi", Url="Uzayresmi.jpg"},
+                new Picture{Id=1,Name="Deniz Resmi", Url="Denizresmi.jpg"}
+            };
+            return Ok(pictures);
+        }
+    }
+}
---
+++ API2/Model/Picture.cs
@@ -0,0 +1,9 @@
+﻿namespace API2.Model
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
