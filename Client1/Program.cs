e51b3cd49f85e89d87205ac788b880f658105e5d
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-01-27 18:34:18 +0300

    Üyelik sistemleri için client 1 auth şemaları hazırlandı.

--- Client1/Program.cs
+++ Client1/Program.cs
@@ -3,6 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(opts =>
+{
+    // Üyelik sistemlerin de 2 türlü üyelik vardýr biri kiþi diðer bayilik için bunu ayýrt etmek için þemalar kullanýlýyor.
+    opts.DefaultScheme = "Cookies";
+    opts.DefaultChallengeScheme = "oidc";
+}).AddCookie("Cookies").AddOpenIdConnect("oidc", opts =>
+{
+    opts.SignInScheme = "Cookies";
+    opts.Authority = "https://localhost:7142";
+    opts.ClientId = "Client1-Mvc";
+    opts.ClientSecret = "password123";
+    opts.ResponseType = "code id_token";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
