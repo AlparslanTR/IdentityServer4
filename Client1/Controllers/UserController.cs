53e073df792fbd7e3b404b08f72c1192d18655a0
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-02-14 19:30:20 +0300

    Refresh Token alma işlemi

--- Client1/Controllers/UserController.cs
+++ Client1/Controllers/UserController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Globalization;
 
 namespace Client1.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
+        private readonly IConfiguration configuration;
+        public UserController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +25,31 @@ namespace Client1.Controllers
         {
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> GetRefreshToken()
+        {
+            HttpClient httpClient = new HttpClient();
+            var discovery = await httpClient.GetDiscoveryDocumentAsync("https://localhost:7142");
+            var refreshToken= await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
+            refreshTokenRequest.ClientId = configuration["Client2:ClientId"];
+            refreshTokenRequest.ClientSecret = configuration["Client2:ClientSecret"];
+            refreshTokenRequest.RefreshToken= refreshToken;
+            refreshTokenRequest.Address = discovery.TokenEndpoint;
+            var token=await httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+            var tokens = new List<AuthenticationToken>()
+            {
+                new AuthenticationToken{Name=OpenIdConnectParameterNames.IdToken,Value=token.IdentityToken},
+                new AuthenticationToken{Name=OpenIdConnectParameterNames.AccessToken,Value=token.AccessToken},
+                new AuthenticationToken{Name=OpenIdConnectParameterNames.RefreshToken,Value=token.RefreshToken},
+                new AuthenticationToken{Name=OpenIdConnectParameterNames.ExpiresIn,Value=DateTime.UtcNow.AddSeconds(token.ExpiresIn).ToString("O",CultureInfo.InvariantCulture)}
+            };
+            var authenticationResult = await HttpContext.AuthenticateAsync();
+            var properties = authenticationResult.Properties;
+            properties.StoreTokens(tokens);
+            await HttpContext.SignInAsync("Cookies",authenticationResult.Principal,properties);
+            return RedirectToAction("Index");
         }
     }
 }
